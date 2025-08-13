9f6f34c9f0d49f4890b22b2304e9ad5d405997cb
alexanderbs3 <alexander.cbss@hotmail.com>
2025-08-13 18:34:21 -0300

    refactor: make GuestUser extend User, remove duplicated code

--- src/main/java/cms/user/GuestUser.java
+++ src/main/java/cms/user/GuestUser.java
@@ -1,4 +1,12 @@
 package cms.user;
 
-public class GuestUser {
+public class GuestUser extends User {
+
+    public GuestUser(int id, String nome, String email, String password) {
+        super(id, nome, email, password);
+    }
+
+    public void viewContent(int contentId) {
+
+    }
 }
