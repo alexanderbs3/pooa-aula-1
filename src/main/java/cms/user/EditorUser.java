482f8b852a5fb2d85b73cb3218510abb5b02f00c
alexanderbs3 <alexander.cbss@hotmail.com>
2025-08-13 18:34:13 -0300

    refactor: make EditorUser extend User, remove duplicated code

--- src/main/java/cms/user/EditorUser.java
+++ src/main/java/cms/user/EditorUser.java
@@ -1,4 +1,12 @@
 package cms.user;
 
-public class EditorUser {
+public class EditorUser extends User {
+
+    public EditorUser(int id, String nome, String email, String password) {
+        super(id, nome, email, password);
+    }
+
+    public void editContent(int contentId, String body) {
+
+    }
 }
